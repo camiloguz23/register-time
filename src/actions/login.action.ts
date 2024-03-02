ee6673aa328d6274cb49730fa2a44b31ae5d3e3c
jeyson <jeysonkmguzman@gmail.com>
2024-03-01 22:08:19 -0500

    fix: console 4

--- src/actions/login.action.ts
+++ src/actions/login.action.ts
@@ -8,7 +8,7 @@ import { redirect } from 'next/navigation';
 export const onLogin = async (data: LoginModel) => {
   await mongodbConnect();
   const response: TimeRegister | null = await timeModel.findOne(data);
-  cookies().set('email', response?.email ?? '');
+  cookies().set('email', response?.nombre ?? '');
   !!response?.email && redirect('/register');
   return false;
 };
