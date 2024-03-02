8a3b17cbd04d876941c571ee48ec4fea635b0720
jeyson <jeysonkmguzman@gmail.com>
2024-03-01 22:57:32 -0500

    fix: enviroment

--- src/app/services/get-user.ts
+++ src/app/services/get-user.ts
@@ -2,7 +2,7 @@ import { TagsEnum, TimeRegister } from '@/shared';
 
 export const getUser = async (email: string) => {
   try {
-    const response = await fetch(`http://localhost:3000/api/time?email=${email}`, { next: { tags: [TagsEnum.register] } });
+    const response = await fetch(`${process.env.HOST}/api/time?email=${email}`, { next: { tags: [TagsEnum.register] } });
     const data: TimeRegister = await response.json();
     return data;
   } catch (error) {
