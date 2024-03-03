fce99e2ce627f0efe6f49a707505e8de91417c67
jeyson <jeysonkmguzman@gmail.com>
2024-03-03 12:39:58 -0500

    fix: register time logic

--- src/app/register/components/button-register/btn-register.tsx
+++ src/app/register/components/button-register/btn-register.tsx
@@ -22,7 +22,7 @@ export const BtnRegister = ({ id, minutesDB }: PropsBtnRegister) => {
   }, []);
 
   const onRegisterTime = async () => {
-    if (storage.value) {
+    if (!storage.value) {
       storage.setStorage('time', `${new Date()}`);
       return;
     }
@@ -30,11 +30,12 @@ export const BtnRegister = ({ id, minutesDB }: PropsBtnRegister) => {
     const result = differenceInMinutes(new Date(), new Date(storage.value));
     const month = new Date().getMonth();
     const setMonth = editMonth(result + minutesDB)[month as CodeMonthType];
+    console.log("🚀 ~ onRegisterTime ~ id, setMonth:", id, setMonth)
     const isSaved = await updateMonthAction(id, setMonth);
     storage.setStorage('time', '');
     isSaved
       ? toast.success('Se registro el Tiempo', { style: { backgroundColor: 'hsla(120,50%,50%,0.4)' } })
-      : toast.error('No se registro el tiempo', { style: { backgroundColor: 'hsla(0,50%,50%,0.4)' } });
+      : toast.error(result ?'No se registro el tiempo': 'tiempo es inferior al minuto no se registra', { style: { backgroundColor: 'hsla(0,50%,50%,0.4)' } });
   };
   return (
     <button onClick={onRegisterTime} className={style.button}>
