65079a6f2c1dfa9b0cd971610b42df385b7e24bc
Camilo Guzman <jeysonkmguzman@gmail.com>
2024-09-03 10:27:05 -0500

    change event blur for onChange

--- src/app/(content)/counter/counter-zoom/ui-counter-zoom.tsx
+++ src/app/(content)/counter/counter-zoom/ui-counter-zoom.tsx
@@ -13,7 +13,7 @@ export function UiCounterZoom(): JSX.Element {
         name=''
         id=''
         placeholder='1 persona'
-        onBlur={(data) => {
+        onChange={(data) => {
           const number: number = Number(data.target.value);
           setAssist((prev) => ({ ...prev, [1]: number * 1 }));
         }}
@@ -24,7 +24,7 @@ export function UiCounterZoom(): JSX.Element {
         name=''
         id=''
         placeholder='2 personas'
-        onBlur={(data) => {
+        onChange={(data) => {
           const number: number = Number(data.target.value);
           setAssist((prev) => ({ ...prev, [2]: number * 2 }));
         }}
@@ -35,7 +35,7 @@ export function UiCounterZoom(): JSX.Element {
         name=''
         id=''
         placeholder='3 personas'
-        onBlur={(data) => {
+        onChange={(data) => {
           const number: number = Number(data.target.value);
           setAssist((prev) => ({ ...prev, [3]: number * 3 }));
         }}
@@ -46,7 +46,7 @@ export function UiCounterZoom(): JSX.Element {
         name=''
         id=''
         placeholder='4 personas'
-        onBlur={(data) => {
+        onChange={(data) => {
           const number: number = Number(data.target.value);
           setAssist((prev) => ({ ...prev, [4]: number * 4 }));
         }}
@@ -57,7 +57,7 @@ export function UiCounterZoom(): JSX.Element {
         name=''
         id=''
         placeholder='5 personas'
-        onBlur={(data) => {
+        onChange={(data) => {
           const number: number = Number(data.target.value);
           setAssist((prev) => ({ ...prev, [5]: number * 5 }));
         }}
@@ -68,7 +68,7 @@ export function UiCounterZoom(): JSX.Element {
         name=''
         id=''
         placeholder='6 personas'
-        onBlur={(data) => {
+        onChange={(data) => {
           const number: number = Number(data.target.value);
           setAssist((prev) => ({ ...prev, [6]: number * 6 }));
         }}
@@ -79,7 +79,7 @@ export function UiCounterZoom(): JSX.Element {
         name=''
         id=''
         placeholder='7 personas'
-        onBlur={(data) => {
+        onChange={(data) => {
           const number: number = Number(data.target.value);
           setAssist((prev) => ({ ...prev, [7]: number * 7 }));
         }}
